7646ed666036ed74906c2a05da5ac8772bd49634
Rishika Shivakumar <53792483+Rishi005@users.noreply.github.com>
2025-11-16 15:08:59 +0200

    Update view_visualizations.py
    Removed commented-out code and updated file paths for opening visualizations.

--- view_visualizations.py
+++ view_visualizations.py
@@ -1,6 +1,4 @@
 import webbrowser
 
-webbrowser.open("/home/rishika/Suomen-Pankki-Junction-2025/risk_dashboard.html")
-# webbrowser.open("/home/rishika/Suomen-Pankki-Junction-2025/similarity_heatmap.html")
-webbrowser.open("/home/rishika/Suomen-Pankki-Junction-2025/network_visualization.html")
-webbrowser.open("/home/rishika/Suomen-Pankki-Junction-2025/3d_animated_network.html")
+webbrowser.open("risk_dashboard.html")
+webbrowser.open("network_visualization.html")
